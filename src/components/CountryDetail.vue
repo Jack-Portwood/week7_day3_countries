d492f1a5876e55ad79e6c43d3b50719950e1e344
Callum Hausrath <callumhausrath@Callum-Hausraths-MacBook-Pro.local>
2020-02-20 08:49:27 +0000

    MVP complete

---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,24 @@
+
+<template lang="html">
+<div v-if='country'>
+<h3>{{country.name}}</h3>
+<p>Name: {{country.name}}</p>
+<p>Capital: {{country.capital}}</p>
+<p>Population: {{country.population}}</p>
+<p v-for="(language, index) in country.languages">Languages: {{language.name}}</p>
+<img :src="country.flag" height="200" width="300"/>
+</div>
+  
+</template>
+
+<script>
+export default {
+    name: 'country-detail',
+    props:['country']
+
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
